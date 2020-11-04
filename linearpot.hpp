8e468c35ab052756d1c81502da7472d9c9afeaa7
Yanze Wu <wuyanze@sas.upenn.edu>
2020-11-03 20:03:27 -0500

    Add LGPL

--- linearpot.hpp
+++ linearpot.hpp
@@ -1,3 +1,25 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// scatter2: 2D scattering calculation Library
+// Copyright (C) 2020 Joseph Subotnik's group, The University of Pennsylvania
+//
+// This file is part of scatter2.
+//
+// scatter2 is free software: you can redistribute it and/or modify it under
+// the terms of the GNU Lesser General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// scatter2 is distributed in the hope that it will be usefull, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+// FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with scatter2. If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include "potential.h"
 
 class BarrierPotential : public Potential {
